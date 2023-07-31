021a779027953e9d60d2f78516bd04d32e9be0f2
Gene Armstrong <garmstrong@underline.com>
2023-07-31 14:48:54 -0700

    Update Kafka implementation to basic interface

--- mb/kafka/kafka.go
+++ mb/kafka/kafka.go
@@ -6,32 +6,20 @@ import (
 	"go-outbox"
 
 	"github.com/twmb/franz-go/pkg/kgo"
-	"google.golang.org/protobuf/proto"
 )
 
 type kafkaMessageBroker struct {
 	client *kgo.Client
 }
 
-var _ outbox.MessageBroker = &kafkaMessageBroker{}
+var _ outbox.MessageBroker[*kgo.Record] = &kafkaMessageBroker{}
 
 func New(client *kgo.Client) (*kafkaMessageBroker, error) {
 	return &kafkaMessageBroker{client}, nil
 }
 
-func (kafkaMessageBroker) EncodeMessage(msg any) ([]byte, error) {
-	protoMsg, ok := msg.(proto.Message)
-	if !ok {
-		return nil, fmt.Errorf("invalid message type: %T", msg)
-	}
-
-	return proto.Marshal(protoMsg)
-}
-
-func (mb kafkaMessageBroker) Send(ctx context.Context, msg []byte) error {
-	record := &kgo.Record{Topic: "foo", Value: msg}
-
-	if err := mb.client.ProduceSync(ctx, record).FirstErr(); err != nil {
+func (mb kafkaMessageBroker) Send(ctx context.Context, msg *kgo.Record) error {
+	if err := mb.client.ProduceSync(ctx, msg).FirstErr(); err != nil {
 		return fmt.Errorf("record had a produce error while synchronously producing: %v", err)
 	}
 
